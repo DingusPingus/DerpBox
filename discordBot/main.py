9c240bb91737f6a252ab434b38894cabee6edc44
DingusPingus <burrito997gaming@gmail.com>
2021-05-04 08:05:21 -0600

    Implemented SQL database
    Added a Counters table that keeps track of certain values, made it so that #nial4life is saved across multiple bot runs because it queries for the value from the database

--- discordBot/main.py
+++ discordBot/main.py
@@ -1,24 +1,28 @@
 import discord
 import logging
+import json
 
 logging.basicConfig(level=logging.INFO)
 client = discord.Client()
-
-
-
+counter = 0
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-nialCounter = 0
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content == '#nial4life':
-        global nialCounter
-        nialCounter += 1
-        await message.channel.send('#nial4life: ' + str(nialCounter))
+        global counter 
+        counter = counter + 1
+        data = {}
+        data['nial4life'] = []
+        data['nial4life'].append({'counter' : counter})
+
+        with open('data.txt', 'w') as outfile:
+            json.dump(data, outfile)
+        await message.channel.send('#nial4life: ' + str(counter) )
 
 client.run('ODM2MzYyMjU4OTA0MzgzNTQ5.YIc5DQ.PW9C_q9Av1oGZMKnEU8W6eB5O7I')
