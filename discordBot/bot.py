e41469122164957d617ac321a1e5a9ee0a102521
DingusPingus <burrito997gaming@gmail.com>
2021-05-07 18:29:36 -0600

    nial counter moved to seperate cog

--- discordBot/bot.py
+++ discordBot/bot.py
@@ -6,17 +6,9 @@ from discord.ext import commands
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = '$', intents = intents)
 
-db = sqlite3.connect('counters.db')
 
-c = db.cursor()
 
-# c.execute("""CREATE TABLE Counters(
-#             name text,
-#             val integer
-#             )""")
 
-# c.execute("""INSERT INTO Counters(name, val) VALUES('#nial4life', 0)""")
-# db.commit()
 
 
 
@@ -38,15 +30,7 @@ for filename in os.listdir('./cogs'):
 
 
 
-@client.command(aliases=['nial4life'])
-async def nial(ctx):
-        nialCounter = c.execute("SELECT * FROM Counters WHERE name = '#nial4life' ").fetchone()[1]
-        nialCounter += 1
-        c.execute("UPDATE Counters SET val =" +str(nialCounter)+ " WHERE name ='#nial4life' ")
-        db.commit()
-        await ctx.send('#nial4life: ' + str(nialCounter))
 
 #TODO implement database for other commands applied to nial
 
 client.run('ODM2MzYyMjU4OTA0MzgzNTQ5.YIc5DQ.PW9C_q9Av1oGZMKnEU8W6eB5O7I')
-db.close()
--- discordBot/cogs/init.py
+++ discordBot/cogs/init.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class Example(commands.Cog):
+class Init(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -13,4 +13,4 @@ class Example(commands.Cog):
     
 
 def setup(client):
-    client.add_cog(Example(client))
+    client.add_cog(Init(client))
