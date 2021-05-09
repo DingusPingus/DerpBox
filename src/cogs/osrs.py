aa96a5e520847717b4115cffa158977dae7d78d5
Dingus Pingus <DingusPingusDev@gmail.com>
2021-05-09 05:31:01 -0400

    removed osrs logo from stats output

--- src/cogs/osrs.py
+++ src/cogs/osrs.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from PIL import Image
 from osrs_api.const import AccountType
 from osrs_api import Hiscores
@@ -8,30 +9,31 @@ class OSRS(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    @commands.command(aliases=['Stats'])
     async def stats(self, ctx, *, username):
         username = str(username)
         user = Hiscores(username)
+        print(user)
         user = user.skills
-        
+        print(user)
+        #/assets/osrsIcons/osrsicons.png
         skills = ('attack', 'hitpoints', 'mining', 'strength', 'agility',
                  'smithing', 'defence', 'herblore', 'fishing', 'ranged',
                  'thieving', 'cooking', 'prayer', 'crafting', 'firemaking',
                  'magic', 'fletching', 'woodcutting', 'runecrafting', 'slayer',
                  'farming','construction','hunter')
         embedVar = discord.Embed(title="OSRS Stats for "+ username)
-        file = discord.File('../assets/osrsIcons/osrsicons.png', filename ='image.png')
-        embedVar.set_image(url='attachment://image.png')
         total = 0
         for i,j in enumerate(skills):
             embedVar.add_field(name=user[j].name, value=user[j].level)
             total +=user[j].level
         embedVar.add_field(name='total', value=total)
 
-        await ctx.send(file=file, embed=embedVar)
-        
-    @commands.command()
+        await ctx.send( embed=embedVar)
+
+    @commands.command(aliases=['Ge', 'GE'])
     async def ge(self, ctx, *, item):
+        item = str(item)
 
 def setup(client):
     client.add_cog(OSRS(client))
