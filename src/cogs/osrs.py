cdd5b7d6d5e808a6e68aa0096a6de31b916d71cc
Dingus Pingus <DingusPingusDev@gmail.com>
2021-05-09 21:57:14 -0400

    added error handling for stats lookup in osrs.py

--- src/cogs/osrs.py
+++ src/cogs/osrs.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import sys
 from PIL import Image
 from osrs_api.const import AccountType
 from osrs_api import Hiscores
@@ -12,27 +13,31 @@ class OSRS(commands.Cog):
     @commands.command(aliases=['Stats'])
     async def stats(self, ctx, *, username):
         username = str(username)
-        user = Hiscores(username)
-        print(user)
-        user = user.skills
-        #/assets/osrsIcons/osrsicons.png
-        skills = ('attack', 'hitpoints', 'mining', 'strength', 'agility',
-                 'smithing', 'defence', 'herblore', 'fishing', 'ranged',
-                 'thieving', 'cooking', 'prayer', 'crafting', 'firemaking',
-                 'magic', 'fletching', 'woodcutting', 'runecrafting', 'slayer',
-                 'farming','construction','hunter')
-        embedVar = discord.Embed(title="OSRS Stats for "+ username)
-        total = 0
-        for i,j in enumerate(skills):
-            embedVar.add_field(name=user[j].name.capitalize(), value=user[j].level)
-            total +=user[j].level
-        embedVar.add_field(name='Total', value=total)
+        try:
+            user = Hiscores(username)
+        except:
+            await ctx.send('There is no player named ' + username)
+        else:
+            user = user.skills
 
-        await ctx.send( embed=embedVar)
+            skills = ('attack', 'hitpoints', 'mining', 'strength', 'agility',
+                    'smithing', 'defence', 'herblore', 'fishing', 'ranged',
+                    'thieving', 'cooking', 'prayer', 'crafting', 'firemaking',
+                    'magic', 'fletching', 'woodcutting', 'runecrafting', 'slayer',
+                    'farming','construction','hunter')
+            embedVar = discord.Embed(title="OSRS Stats for "+ username)
+            total = 0
+            for i,j in enumerate(skills):
+                embedVar.add_field(name=user[j].name.capitalize(), value=user[j].level)
+                total +=user[j].level
+            embedVar.add_field(name='Total', value=total)
+
+            await ctx.send( embed=embedVar)
 
     @commands.command(aliases=['Ge', 'GE'])
     async def ge(self, ctx, *, item):
         item = str(item)
+        #TODO add grand exchange embed output from osrs api
 
 def setup(client):
     client.add_cog(OSRS(client))
